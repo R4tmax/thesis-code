399ebf7142723f346539d3d793bdc39827b7a295
R4tmax <kadlec.m.90@gmail.com>
2026-03-08 17:56:40 +0100

    Instantiate wif and initial flows

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -8,7 +8,6 @@ module "state_bucket" {
   source = "../../modules/state_bucket"
 
   project_id  = var.dev_proj_id
-  #bucket_name = "${var.dev-proj_designation}_${var.dev_naming_prefix}_state_bucket" # note that buckets exist in public&global namespace
   bucket_name = "${var.dev_proj_id}_state_bucket"
   location    = "EUROPE-WEST3"
 }
---
+++ infrastructure/environments/dev/wif.tf
@@ -0,0 +1,52 @@
+resource "google_service_account" "github_actions_sa" {
+  account_id   = "github-actions-sa"
+  display_name = "Service Account for GitHub Actions CI/CD"
+}
+
+resource "google_project_iam_member" "sa_default_role" {
+  project = var.dev_proj_id
+  role    = "roles/reader"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
+}
+
+resource "google_iam_workload_identity_pool" "github_pool" {
+  workload_identity_pool_id = "github-actions-pool"
+  display_name              = "GitHub Actions Pool"
+  description               = "Identity pool for GitHub Actions deployments"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  display_name                       = "GitHub Provider"
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.repository" = "assertion.repository"
+  }
+
+  attribute_condition = "assertion.repository == 'R4tmax/thesis-code'"
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account_iam_member" "github_actions_sa_binding" {
+  service_account_id = google_service_account.github_actions_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/R4tmax/thesis-code"
+}
+
+
+output "github_actions_provider_name" {
+  value       = google_iam_workload_identity_pool_provider.github_provider.name
+  description = "The Workload Identity Provider name to put in your GitHub Actions YAML"
+}
+
+output "github_actions_sa_email" {
+  value       = google_service_account.github_actions_sa.email
+  description = "The Service Account email to put in your GitHub Actions YAML"
+}
